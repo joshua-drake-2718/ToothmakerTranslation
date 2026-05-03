18cae0bd935e0140b0097955135631d062615567
Lyndon Drake <lyndon@arotau.com>
2026-05-03 16:27:32 +0100

    refactor: convert vector.py to plain numpy (0-based)

--- vector.py
+++ vector.py
@@ -1,12 +1,12 @@
 import math
-from indexing import float_array
+import numpy as np
 
 def magnitude(x: float, y: float, z: float) -> float:
     return math.sqrt(x*x + y*y + z*z)
 
 def a_magnitude(v) -> float:
-    if isinstance(v, float_array):
-        v = [v[1], v[2], v[3]]
+    if isinstance(v, np.ndarray):
+        return magnitude(v[0], v[1], v[2])
     return magnitude(v[0], v[1], v[2])
 
 def distance_between(
@@ -16,25 +16,17 @@ def distance_between(
     return magnitude(x1-x2, y1-y2, z1-z2)
 
 def a_distance_between(u, v) -> float:
-    if isinstance(u, float_array):
-        u = [u[1], u[2], u[3]]
-    if isinstance(v, float_array):
-        v = [v[1], v[2], v[3]]
     return distance_between(u[0], u[1], u[2], v[0], v[1], v[2])
 
 def cross_product(
     x1: float, y1: float, z1: float,
     x2: float, y2: float, z2: float,
 ):
-    return float_array(buffer=[
+    return np.array([
         y1 * z2 - z1 * y2,
         z1 * x2 - x1 * z2,
         x1 * y2 - y1 * x2,
     ])
 
-def a_cross_product(u, v) -> float_array:
-    if isinstance(u, float_array):
-        u = [u[1], u[2], u[3]]
-    if isinstance(v, float_array):
-        v = [v[1], v[2], v[3]]
+def a_cross_product(u, v) -> np.ndarray:
     return cross_product(u[0], u[1], u[2], v[0], v[1], v[2])
